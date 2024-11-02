97faa4b773a2ee9b248ea8d169ca046d38fb00b1
Shubham Juyal <shubhamjuyal4444@gmail.com>
2024-11-02 07:19:37 +0530

    Update main.tf

--- proj/main.tf
+++ proj/main.tf
@@ -6,3 +6,16 @@ module "data_source_bucket" {
   csv_file_path           = var.csv_file_path
   csv_name                = var.csv_name
 }
+
+module "glue_db" {
+  source             = "./modules/glue_db"
+  glue_database_name = var.glue_database_name
+}
+
+module "crawler" {
+  source                  = "./modules/crawler"
+  data_source_bucket_name = var.data_source_bucket_name
+  tableName               = var.tableName
+  partitioner             = var.partitioner
+  glue_database_name      = var.glue_database_name
+}

26f78a65b4b7ad86729ab7e0890d451630c07f1b
Shubham Juyal <shubhamjuyal4444@gmail.com>
2025-01-03 16:43:13 +0530

    create glue iam

--- proj/modules/iam/main.tf
+++ proj/modules/iam/main.tf
@@ -1,5 +1,6 @@
+# < CRAWLER IAM > #
 resource "aws_iam_role" "crawler_role" {
-  name = "GlueExecutionRole"
+  name = "CrawlerExecutionRole"
   assume_role_policy = jsonencode({
     Version : "2012-10-17"
     Statement : [
@@ -28,3 +29,39 @@ resource "aws_iam_role_policy_attachment" "glue_service" {
   role       = aws_iam_role.crawler_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
+
+# < / CRAWLER IAM > #
+
+# < ETL GLUE IAM > #
+resource "aws_iam_role" "glue_role" {
+  name = "GlueJobRole"
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17"
+    Statement : [
+      {
+        Effect : "Allow"
+        Principal : {
+          Service : "glue.amazonaws.com"
+        }
+        Action : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "s3" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "glue_service" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+# < / ETL GLUE IAM > #
